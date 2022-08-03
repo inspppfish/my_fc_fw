2aed11b55fee5fd063b97bd9dd6a980a3d721daa
insppp <freefish_zhou@foxmail.com>
2022-08-03 10:44:16 +0800

    BMI088 Ok

---
+++ Core/Inc/struct_typedef.h
@@ -0,0 +1,14 @@
+//
+// Created by insppp on 2022/8/3.
+//
+
+#ifndef MY_FC_FW_STRUCT_TYPEDEF_H
+#define MY_FC_FW_STRUCT_TYPEDEF_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef float                fp32;
+typedef bool                 bool_t;
+
+#endif //MY_FC_FW_STRUCT_TYPEDEF_H
